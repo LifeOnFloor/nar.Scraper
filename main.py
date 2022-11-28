484469c4710331bbffeda24583245115b56fd7a5
LifeOnFloor <119148510+LifeOnFloor@users.noreply.github.com>
2022-11-29 04:09:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,129 +1,239 @@
 import lxml
 import time
+import csv
+import os
 from bs4 import BeautifulSoup
-import urllib.request, urllib.error
 from datetime import datetime, timedelta
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-##############
+
+####################
+####
+####    requests
+####
+####################
+
+# 馬番を併記したい
+
+####################
+####
+####    var List
+####
+####################
+
+# url='地方競馬のトップページ'
 #
-#　設定
+# tomorrow='明日の日付'
+# URL_tomorrow='明日のURL'
 #
-##############
-
-# [良,稍,重,不]を選択
-TARGET_CONDITION = '良'
-
-
-##############
+# name_track='開催日の開催場'
+# url_track='開催日の開催場ごとのURL'
 #
-#　本体
+# num='開催場のレース番号'
+# url_num='開催場のレースURL'
 #
-##############
+# TARGET_distance='今走のレース条件（ダ1400など）'
+# horse='今走の馬名'
+# URL_horse='今走の馬ページURL '
+#
+# track='result track'
+# distance='result distance'
+# condition='result condition'
+# date='result date'
+# result='result time'
+#
 
-URL = 'https://nar.netkeiba.com/'
-driver = webdriver.Chrome()
+
+
+####################
+####
+####    Setting
+####
+####################
+
+url = 'https://nar.netkeiba.com/'
+
+# select race condition [Default:良,2:稍,3:重,4:不]
+TARGET_condition = input('input 2 or 3 or 4 or Enter as 1: ')
+if TARGET_condition == 2:
+    TARGET_condition = '稍'
+elif TARGET_condition == 3:
+    TARGET_condition = '重'
+elif TARGET_condition == 4:
+    TARGET_condition = '不'
+else:
+    TARGET_condition = '良'
+
+# select race_date
+TARGET_day = input('input yyyymmdd or Enter as tomorrow: ')
+today = datetime.today()
+tomorrow = today +timedelta(days=1)
+if len(TARGET_day) == 0:
+    race_day = datetime.strftime(tomorrow, '%Y%m%d')
+else:
+    race_day = TARGET_day
+print('Get race day : ' + race_day)
 
 
 
-# 明日の開催場所トップページへ遷移する
-TODAY = datetime.today()
-TOMORROW = TODAY +timedelta(days=1)
+####################
+####
+####    Function
+####
+####################
 
-URL_TOMORROW = URL + '?kaisai_date=' + datetime.strftime(TOMORROW, '%Y%m%d') + '&rf=race_list'
+def waitForLoad(driver):
+    i = 0
+    elem = driver.find_elements(By.TAG_NAME, "html")
+    while True:
+        if len(elem) > 0:
+            time.sleep(1)
+            return
+        if i == 20:
+            print('waited 10s')
+            return
+        else:
+            i += 1
+            time.sleep(.5)
 
-print('Get Date:',datetime.strftime(TOMORROW, '%Y/%m/%d'))
 
-driver.get(URL_TOMORROW)
+
+####################
+###
+###    driver setting
+###
+####################
+
+# option
+options = Options()
+options.add_argument("--headless")
+options.add_argument("log-level=3")
+options.add_argument("disable-logging")
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+# get driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), service_log_path = "NUL", options = options)
+
+
+
+####################
+###
+###    main
+###
+####################
+
+##### jump to/target/date
+
+driver.get(url + '?kaisai_date=' + race_day + '&rf=race_list')
 html = driver.page_source
 soup = BeautifulSoup(html, 'lxml')
 
-WebDriverWait(driver, 10)
+waitForLoad(driver)
 
 
 
-# 開催されている競馬場をひとつずつ選択する
+##### select the track
 
-TRACKS = soup.select("ul.RaceList_ProvinceSelect > li > a")
+TRACKs = soup.select("ul.RaceList_ProvinceSelect > li > a")
 
-URL_TRACK = [ i.get("href") for i in TRACKS ]
-NAME_TRACK = [ i.getText() for i  in TRACKS ]
+URLs_track = [i.get("href") for i in TRACKs]
+NAMEs_track = [i.getText() for i  in TRACKs]
 
-for (name,url_loc) in zip(NAME_TRACK, URL_TRACK):
-    driver.get(URL + url_loc)
-    WebDriverWait(driver, 10)
-    time.sleep(3)
+for (name_track, url_track) in zip(NAMEs_track, URLs_track):
 
+    driver.get(url + url_track)
+    waitForLoad(driver)
 
-
-    # レースをひとつずつ選択する
-
-    RACE = soup.select("li.RaceList_DataItem > a")
-    RACE_NUMS = soup.select("li.RaceList_DataItem > a > div.Race_Num > span")
-
-    RACE_NUM = [ i.getText() for i in RACE_NUMS ]
-    URL_NUM = [ i.get("href") for i in RACE ]
-
-    for num,url_num in zip(RACE_NUM, URL_NUM):
-        url_num = url_num[3:]
-        print(name , num, URL + url_num)
-        driver.get(URL + url_num)
-        WebDriverWait(driver, 10)
-        time.sleep(3)
+    html_track = driver.page_source
+    soup_track = BeautifulSoup(html_track, 'lxml')
 
 
 
-        # レース条件を取得する
+    ##### select the race
+
+    RACEs = soup_track.select("li.RaceList_DataItem > a")
+    LISTs_num = soup_track.select("li.RaceList_DataItem > a > div.Race_Num > span")
+
+    LIST_num = [i.getText() for i in LISTs_num]
+    LIST_urls_num = [i.get("href") for i in RACEs]
+    urls_num = [i for i in LIST_urls_num  if i[:12] != '../race/movie']
+
+    for num,url_num in zip(LIST_num, urls_num):
+        if url_num[:15] == '../race/shutuba':
+            url_num = url_num[2:]
+            
+        driver.get(url + url_num)
+        waitForLoad(driver)
 
         html_horse = driver.page_source
         soup_horse = BeautifulSoup(html_horse, 'lxml')
-        TARGET_DISTANCE = soup_horse.select_one("div.RaceData01 > span").getText()[1:-1]
-        TARGET_TRACK = soup_horse.select("div.RaceData02 > span")[1].getText()
-        print(TARGET_TRACK)
 
 
 
-        # 馬を1頭ずつ選択する
+        ##### get the race info
 
-        HORSES_LIST = soup_horse.select("span.HorseName > a")
-        HORSES = [ i.getText() for i in HORSES_LIST ]
-        URL_HORSES = [ i.get("href") for i in HORSES_LIST ]
+        TARGET_distance = soup_horse.select_one("div.RaceData01 > span").getText()[1:-1]
 
-        for HORSE, URL_HORSE in zip(HORSES, URL_HORSES):
-            driver.get(URL_HORSE)
 
-            WebDriverWait(driver, 10)
-            time.sleep(3)
             
+        ##### create new file.csv
+        if os.path.isdir('log/'+race_day+'/'+name_track) == False:
+            os.makedirs('log/'+race_day+'/'+name_track)
+
+        with open('log/'+race_day+'/'+name_track+'/'+race_day+'_'+name_track+'_'+num+'.csv', 'w', newline='', encoding='UTF-8') as f:
+            write = csv.writer(f)
+            write.writerow(['track + num', 'distance', 'condition', 'horse', 'result_day', 'result_time'])
 
 
-            # 戦績を取得する
 
+        ##### select the horse
+
+        LISTs_horse = soup_horse.select("span.HorseName > a")
+        LIST_horse = [i.getText() for i in LISTs_horse]
+        urls_horse = [i.get("href") for i in LISTs_horse]
+
+
+
+        ##### get the horse
+
+        for horse, url_horse in zip(LIST_horse, urls_horse):
+            driver.get(url_horse)
+            waitForLoad(driver)
             html_result = driver.page_source
             soup_horse = BeautifulSoup(html_result, 'lxml')
 
-            RESULTS_LIST = soup_horse.select('table.db_h_race_results > tbody > tr > td')
+
+
+            ##### get the results
+
+            LISTs = soup_horse.select('table.db_h_race_results > tbody > tr > td')
             
-            RESULTS_LIST_TRACK = RESULTS_LIST[1::28]
-            RESULTS_LIST_DATE = RESULTS_LIST[0::28]
-            RESULTS_LIST_DISTANCE = RESULTS_LIST[14::28]
-            RESULTS_LISTS_CONDITION = RESULTS_LIST[15::28]
-            RESULTS_LIST_TIME = RESULTS_LIST[17::28]
+            LISTs_track = LISTs[1::28]
+            LISTs_race_day = LISTs[0::28]
+            LISTs_distance = LISTs[14::28]
+            LISTs_condition = LISTs[15::28]
+            LISTs_time = LISTs[17::28]
 
-            RESULTS_TRACK = [i.getText() for i in RESULTS_LIST_TRACK]
-            RESULTS_DATE = [i.getText() for i in RESULTS_LIST_DATE]
-            RESULTS_CONDITION = [i.getText() for i in RESULTS_LISTS_CONDITION]
-            RESULTS_DISTANCE = [i.getText() for i in RESULTS_LIST_DISTANCE]
-            RESULTS_TIME = [i.getText() for i in RESULTS_LIST_TIME]
+            LIST_track = [i.getText() for i in LISTs_track]
+            LIST_race_day = [i.getText() for i in LISTs_race_day]
+            LIST_condition = [i.getText() for i in LISTs_condition]
+            LIST_distance = [i.getText() for i in LISTs_distance]
+            LIST_time = [i.getText() for i in LISTs_time]
 
 
 
-            # 該当競馬場、該当距離、該当馬場状態（デフォルト：良）の走破タイムを出力
+            ##### output results to file.csv
 
-            for RESULT_TRACK, RESULT_DATE, RESULT_CONDITION, RESULT_DISTANCE, RESULT_TIME in zip(RESULTS_TRACK, RESULTS_DATE, RESULTS_CONDITION, RESULTS_DISTANCE, RESULTS_TIME):
-                if RESULT_TRACK == TARGET_TRACK and RESULT_DISTANCE == TARGET_DISTANCE and RESULT_CONDITION == TARGET_CONDITION:
-                    print(RACE_NUM, RESULT_TRACK, HORSE, RESULT_DISTANCE, RESULT_CONDITION, RESULT_DATE, RESULT_TIME)
+            print(name_track, num, horse)
 
-            WebDriverWait(driver, 10)
-            time.sleep(1)
+            for track, distance, condition, result_day, result in zip(LIST_track, LIST_distance, LIST_condition, LIST_race_day, LIST_time):
+
+                if track == name_track and distance == TARGET_distance and condition == TARGET_condition and len(result) != 0:
+                    print(name_track + num, distance, condition, horse, result_day, result)
+                    with open('log/'+race_day+'/'+track+'/'+race_day+'_'+name_track+'_'+num+'.csv', 'a', newline='', encoding='UTF-8') as f:
+                        write = csv.writer(f)
+                        write.writerow([name_track + num, distance, condition, horse, result_day, result])
